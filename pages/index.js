1382b46e4e16bc602728d373c1669742c7a7eb18
halosatrio <loredana@disroot.org>
2021-01-29 17:01:43 +0700

    01. time tracking nav + hero responsive

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,74 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Tailwind Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <header id="hero" className="lg:h-screen w-full bg-gray-500">
+        <nav className="max-w-screen-xl mx-auto px-4 grid grid-cols-6 md:grid-cols-12 gap-2 sm:gap-4 pt-10 items-center">
+          <div className="col-span-2">
+            <Image
+              src="/assets/Logo.png"
+              alt="Logo"
+              className="mx-4 w-full relative"
+              width={138}
+              height={43}
+            />
+          </div>
+          <ul className="md:col-start-4 lg:col-start-6 xl:col-start-7 col-span-4 sm:col-span-3  md:col-span-9 lg:col-span-7 xl:col-span-6 flex justify-end text-xl text-dark-200 font-medium">
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2">
+              Feature
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2">
+              Pricing
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2">
+              Why Gravity
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2 text-teal-900">
+              Sign In
+            </li>
+          </ul>
+        </nav>
+        <div className="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-4 mt-12 md:mt-20  justify-evenly md:justify-items-center   max-w-screen-xl mx-auto px-4 lg:items-center pb-24 md:pb-0">
+          <div className="w-8/12 md:w-10/12  lg:w-full mx-auto flex flex-col justify-start items-start text-dark-500 mt-12 lg:mt-0 px-4 lg:px-0">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl leading-relaxed">
+              Simple time tracking.
+              <br /> Powerful reporting.
+            </h1>
+            <p className="my-6 text-base md:text-xl lg:text-2xl">
+              Turn your team on to productivity with Gravity the time tracker.
             </p>
-          </a>
+            <label
+              htmlFor="email"
+              className="flex flex-col md:flex-row w-full items-start md:items-center"
+            >
+              <input
+                type="text"
+                id="email"
+                className="text-dark-500 bg-transparent border-2 w-full md:w-7/12 border-black rounded-lg shadow-sm py-3 px-4 md:text-lg"
+              />
+              <button className="md:text-lg font-bold border-2 border-black rounded-lg shadow py-3 px-4 bg-teal-900 text-white mt-4 md:mt-0 md:mx-4 hover:bg-teal-600 hover:border-teal-500 transition delay-200">
+                Request early access
+              </button>
+            </label>
+          </div>
+          <div className="flex items-center justify-start sm:justify-center row-start-1 w-10/12 lg:w-auto lg:col-start-2 mx-auto">
+            <Image
+              src="/assets/Illustration.png"
+              width={620}
+              height={664}
+              alt="Illustration Time Tracking"
+              className="w-full md:w-9/12 lg:w-10/12 flex justify-start"
+            />
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </header>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: {
+          500: "hsl(169,24%,55%)",
+          600: "hsl(175,24%,55%)",
+          700: "hsl(180,24%,55%)",
+          800: "hsl(205,24%,55%)",
+          900: "hsl(205,24%,42%)",
+        },
+        gray: {
+          500: "hsl(0,0%,98%)",
+        },
+        dark: {
+          200: "hsl(240,1%,19%)",
+          500: "hsl(169,19%,16%)",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
