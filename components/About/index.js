314fbdd93d821abc3d218219174abb4e50f7daad
halosatrio <loredana@disroot.org>
2021-02-03 16:14:18 +0700

    02. change asset

--- components/About/index.js
+++ components/About/index.js
@@ -3,15 +3,15 @@ const About = () => {
     <div className="w-full my-7 lg:w-4/5 md:mx-auto mt-7 lg:mb-12 grid grid-rows-1 md:grid-cols-2 gap-y-5 md:gap-x-7 lg:gap-x-12">
       <img
         className="px-4 md:pr-0 md:pl-4 max-w-full bg-cover"
-        src="/assets/satrio.jpg"
+        src="/assets/foto.jpg"
         alt="satrio bayu aji"
       />
       <div className="font-sans text-base max-w-full xl:max-w-4/5 font-thin leading-6 pt-2 lg:pt-3 xl:pt-5 px-4 md:pl-0 md:pr-4">
         <p>
-          My name is Satrio. I am a recent Graduate of Universitas Gadjah Mada.
-          I am a Front End Developer / Data Analyst. living in Yogyakarta,
-          Indonesia. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Ab, rerum?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio,
+          et eum. Impedit cupiditate pariatur omnis. Vel a autem quia quos,
+          labore ullam, vitae quod cupiditate porro provident, repudiandae eius
+          at in eaque. Ad cupiditate fuga minima, illo dolorum pariatur unde.
         </p>
         <br />
         <p>
